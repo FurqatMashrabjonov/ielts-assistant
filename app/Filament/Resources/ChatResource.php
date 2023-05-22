ce3038801d6256314c59ecf2717e97aa0ef1bbeb
FurqatMashrabjonov <php_lesson@mail.ru>
2023-05-22 14:13:33 +0500

    Chat Resource Filament

---
+++ app/Filament/Resources/ChatResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ChatResource\Pages;
+use App\Filament\Resources\ChatResource\RelationManagers;
+use App\Models\Chat;
+use DefStudio\Telegraph\Models\TelegraphChat;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ChatResource extends Resource
+{
+    protected static ?string $model = TelegraphChat::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('chat_id')->searchable(),
+                Tables\Columns\TextColumn::make('name')->searchable()->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListChats::route('/'),
+            'create' => Pages\CreateChat::route('/create'),
+            'edit' => Pages\EditChat::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Filament/Services/NavigationService.php
+++ app/Filament/Services/NavigationService.php
@@ -4,11 +4,13 @@ namespace App\Filament\Services;
 
 use App\Enums\UserRole;
 use App\Filament\Resources\CambridgeResource;
+use App\Filament\Resources\ChatResource;
 use App\Filament\Resources\SpeakingAnswerResource;
 use App\Filament\Resources\SpeakingCategoryResource;
 use App\Filament\Resources\SpeakingIdeaResource;
 use App\Filament\Resources\SpeakingQuestionResource;
 use App\Filament\Resources\UserResource;
+use DefStudio\Telegraph\DTO\Chat;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationBuilder;
 use Filament\Navigation\NavigationGroup;
@@ -18,34 +20,35 @@ class NavigationService
 
     protected static function getItems(): array
     {
-            $speakingItems = [
-                ...SpeakingCategoryResource::getNavigationItems(),
-                ...SpeakingAnswerResource::getNavigationItems(),
-                ...SpeakingIdeaResource::getNavigationItems(),
-                ...SpeakingQuestionResource::getNavigationItems()
-            ];
+        $speakingItems = [
+            ...SpeakingCategoryResource::getNavigationItems(),
+            ...SpeakingAnswerResource::getNavigationItems(),
+            ...SpeakingIdeaResource::getNavigationItems(),
+            ...SpeakingQuestionResource::getNavigationItems()
+        ];
 
-            $generalItems = [
-                ...UserResource::getNavigationItems()
-            ];
+        $generalItems = [
+            ...UserResource::getNavigationItems()
+        ];
 
-            $cambridgeItems = [
-              ...CambridgeResource::getNavigationItems()
-            ];
+        $cambridgeItems = [
+            ...CambridgeResource::getNavigationItems(),
+            ...ChatResource::getNavigationItems()
+        ];
 
-            return match (auth()->user()->role){
-                UserRole::MODERATOR => [
-                    NavigationGroup::make('Speaking')
-                        ->items($speakingItems)],
-                UserRole::SUPER_ADMIN => [
-                    NavigationGroup::make('General')
-                        ->items($generalItems),
-                    NavigationGroup::make('Speaking')
-                        ->items($speakingItems),
-                    NavigationGroup::make('Cambridge')
-                        ->items($cambridgeItems),
-                ]
-            };
+        return match (auth()->user()->role) {
+            UserRole::MODERATOR => [
+                NavigationGroup::make('Speaking')
+                    ->items($speakingItems)],
+            UserRole::SUPER_ADMIN => [
+                NavigationGroup::make('General')
+                    ->items($generalItems),
+                NavigationGroup::make('Speaking')
+                    ->items($speakingItems),
+                NavigationGroup::make('Cambridge')
+                    ->items($cambridgeItems),
+            ]
+        };
     }
 
     public static function navigator()
